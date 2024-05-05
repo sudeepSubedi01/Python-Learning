ece07e72d7005eeb17eecb1fb7a4e743b7195468
Sudip Subedi <sudip.subedi99@gmail.com>
2024-05-05 20:09:08 +0545

    updated random

--- 020 RandomMethod.py
+++ 020 RandomMethod.py
@@ -2,8 +2,6 @@ import random
 
 print(random.randint(1,5))  #returns a random integer number
 print(random.random())      #returns a random number between 0 and 1
-# print(random.randrange(1,15,2))
-
 
 myList = ['rock', 'paper','scissors']
 print(random.choice(myList))
@@ -11,3 +9,11 @@ print(random.choice(myList))
 cards = [1,2,3,4,5,6,7,8,9,'J','K','Q','A']
 random.shuffle(cards)
 print(cards)
+
+dict = {
+  'hello' : 'world',
+  'this' : 'browser',
+  'my':'home',
+  'react' : 'js'
+}
+print(random.choice(list(dict.items())))
