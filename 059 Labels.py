975defcd0b59763946eb69d5c68ee682a8ca4128
Sudip Subedi <sudip.subedi99@gmail.com>
2024-05-22 12:18:31 +0545

    labels

---
+++ 059 Labels.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+window = Tk()
+window.geometry('900x420')
+photo = PhotoImage(file = 'python-logo.png')
+
+label = Label(window,
+              text = 'hello world, this is my new home from now on',
+              font=('Arial',40,'bold'), 
+              fg = '#00FF00', 
+              # bg='black',
+              relief =RAISED,
+              bd = 10,
+              padx = 20,
+              pady = 20,
+              image = photo,
+              compound = 'bottom')
+label.pack()    #places label at the center of the window
+# label.place(x=100,y=100)
+window.mainloop()
+print(type(window))     #type of window is tkinter
